5e9aa0e30cdce50d3a9554c5c73bebd871814fc7
rbhatt199924 <rbhatt199924@gmail.com>
2022-12-06 19:17:57 +0530

    webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname,'dist'),
+    },
+    open: true,
+    hot: true,
+    compress: true,
+    port:5000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack exercise',
+      template: './src/index.html',
+    }),
+  ],
+};
